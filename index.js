dd73ab37b317ee8a815e7901b14a37a9af6c803e
David Chapman <chap6680misc@gmail.com>
2018-01-20 02:41:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  if (item !== undefined) {
+//   cart[item] = Math.floor(Math.random() * 100);
+    cart.push({ [item]: Math.floor(Math.random() * 100) });
+   console.log(`${item} has been added to your cart.`);
+   return cart;
+  }
 }
 
 function viewCart() {
